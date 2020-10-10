5c719289c6bf70203ffcc4b880995e0886624dc1
Sergio Olvera <solvera533@gmail.com>
2020-10-10 11:07:55 -0600

    New python files

---
+++ currency.py
@@ -0,0 +1,25 @@
+"""
+Module for currency exchange
+
+This module provides several string parsing functions to implement a simple
+currency exchange routine using an online currency service. The primary function
+in this module is exchange().
+
+Author: Sergio Olvera
+Date:   10/10/2020
+"""
+import introcs
+
+APIKEY = 'H2u53AGBwvuiofitARa0lKQgnnkNThgrV8hn3XGxYT0e'
+
+
+def before_space(s):
+    """
+    Returns the substring of s up to, but not including, the first space.
+
+    Example: before_space('Hello World') returns 'Hello'
+
+    Parameter s: the string to slice
+    Precondition: s is a string with at least one space in it
+    """
+    pass
---
+++ exchangeit.py
@@ -0,0 +1,10 @@
+"""
+User interface for module currency
+
+When run as a script, this module prompts the user for two currencies and amount.
+It prints out the result of converting the first currency to the second.
+
+Author: Sergio Olvera
+Date:   10/10/2020
+"""
+import currency
